05388ccd6cff1d94fd8d22af74a2dcdd7cba2cc9
Angelo René Acosta Castellanos <aacosta@belatrix.com>
2020-01-10 15:05:31 -0500

    moved to git

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,15 @@
-import '../styles/index.scss';
+import $ from 'jquery';
 
-console.log('webpack starterkit');
+let user = {
+    name: 'Angelo',
+    avatar: 'angelo.jpg'
+};
+
+let promise = $.post(
+    'http://131515149131.mockapi.io/api/v1/users', user
+);
+
+promise.then(
+    data => console.log('success: ', data),
+    error => console.log('error: ', error)
+);
